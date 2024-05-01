b5683ed9ed70cf894d14fac246dde903cdc869ae
mhicks110 <mhicks110@academic.rrc.ca>
2024-05-01 14:10:40 -0500

    Added index and sandbox for tutorial 2
    Tutorial 2

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,20 @@
+// alert('hello, world');
+
+// runs top to bottom
+// console.log(1);
+// console.log(2);
+
+// log things to console
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50; // constants not overwritten
+console.log(points);
+
+var score = 75;
+console.log(score);
